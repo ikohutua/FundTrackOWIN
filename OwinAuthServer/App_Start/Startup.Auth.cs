c3b6de610304c7a049689587fca7729d60fa6428
Andriy Yavorskiy <andriy.yavorskiy21@gmail.com>
2017-10-09 23:43:34 +0300

    Add project files.

---
+++ OwinAuthServer/App_Start/Startup.Auth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using WebApplicationOwin.Provider;
+
+namespace OwinAuthServer
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+        public Startup()
+        {
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                //sets the URL by which the client will receive the token
+                TokenEndpointPath = new PathString("/token"),
+                Provider = new OAuthAppProvider(),
+
+                //It indicates the route by which the user will be redirected for authorization.
+                AuthorizeEndpointPath = new PathString("/api/User/LogIn"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(2),
+                AllowInsecureHttp = true
+            };
+        }
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseOAuthBearerTokens(OAuthOptions);
+        }
+    }
+}
---
+++ OwinAuthServer/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using WebApplicationOwin;
+
+[assembly: OwinStartup(typeof(OwinAuthServer.Startup))]
+
+namespace OwinAuthServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
