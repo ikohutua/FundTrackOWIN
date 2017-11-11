d5e0766564b272ba53ff595efdd6fb1d4950ac2c
Andriy Yavorskiy <andriy.yavorskiy21@gmail.com>
2017-11-11 19:44:47 +0200

    changed package version

--- OwinAuthServer/App_Start/WebApiConfig.cs
+++ OwinAuthServer/App_Start/WebApiConfig.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http;
-using System.Web.Http.Cors;
 
 namespace WebApplicationOwin
 {
